3041598b2bd0bcca09f8a91252aa2f76505bb843
SanishKumar <18sanishkumar@gmail.com>
2023-09-23 23:45:38 +0530

    File Uploaded!

---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+
+export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        <Router>
+            <Routes>
+                <Route path='/join' element={<RoomJoinPage />}/>
+                <Route path='/create' element={<CreateRoomPage />}/>
+                <Route path='/' element={<p> This is the Home Page </p>}/>
+            </Routes>
+        </Router>
+    );
+  }
+}
